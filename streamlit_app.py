1f474051f32eb8785264b61fe9a5f53807d644a4
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-09-01 15:39:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,21 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+from transformers import pipeline
+import secrets
+um = pipeline("fill-mask", model="bert-base-cased")
+unmask = lambda x: um(x)[0]["sequence"]
+def fill_masks(text):
+    text = text.replace("[MASK]", "[UNK]")
+    for _ in range(text.count("[UNK]")):
+        text = unmask(text.replace("[UNK]", "[MASK]", 1))
+    return text.replace('##', '')
+def mask_corrupt(text, num):
+    for _ in range(num):
+        m = list(text)
+        for x in range(len(m)):
+            if secrets.randbits(1):
+                m[x] = "[MASK]"
+        text = fill_masks(text)
+    return text
+text = st.text_area("Text to mask corrupt", "A ble con s j jd tin")
+num = st.number_input("Number of iterations", 1)
+st.button("Mask Corrupt", on_click = lambda: st.write(mask_corrupt(text, num)))
